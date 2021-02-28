ba63e33fb1d2071c712fca06df1bb2b2d991e5b0
Roma Labunskiy <romalabunskiy13@gmail.com>
2021-02-28 11:20:03 +0200

    рабочая версия 1

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,7 +7,7 @@ import sys
 
 
 class App(QWidget):
-
+    
     def __init__(self):
 
         self.ui = uic.loadUi("untitled.ui")
@@ -16,15 +16,23 @@ class App(QWidget):
 
         self.cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
-        self.a = 0
         self.list_btn = [self.ui.btn1, self.ui.btn2, self.ui.btn3,
                          self.ui.btn4, self.ui.btn5, self.ui.btn6,
                          self.ui.btn7, self.ui.btn8, self.ui.btn9]
 
-        self.a = 0
+        self.counter = 0
 
-        self.click()
-
+        self.ui.btn1.clicked.connect(lambda: self.game(0, 0, self.ui.btn1))
+        self.ui.btn2.clicked.connect(lambda: self.game(0, 1, self.ui.btn2))
+        self.ui.btn3.clicked.connect(lambda: self.game(0, 2, self.ui.btn3))
+        self.ui.btn4.clicked.connect(lambda: self.game(1, 0, self.ui.btn4))
+        self.ui.btn5.clicked.connect(lambda: self.game(1, 1, self.ui.btn5))
+        self.ui.btn6.clicked.connect(lambda: self.game(1, 2, self.ui.btn6))
+        self.ui.btn7.clicked.connect(lambda: self.game(2, 0, self.ui.btn7))
+        self.ui.btn8.clicked.connect(lambda: self.game(2, 1, self.ui.btn8))
+        self.ui.btn9.clicked.connect(lambda: self.game(2, 2, self.ui.btn9))
+        self.ui.btn_reset.clicked.connect(lambda: self.reset())
+    
     def is_winner(self, player):
         winner = player*3 
 
@@ -43,60 +51,43 @@ class App(QWidget):
             return True
 
         return False
-
+    
     def is_tie(self):
         for row in self.cells:
             if " " in row:
                 return False
         return True
     
-    def click(self):
-        self.ui.btn1.clicked.connect(lambda: self.game(0, 0, self.ui.btn1))
-        self.ui.btn2.clicked.connect(lambda: self.game(0, 1, self.ui.btn2))
-        self.ui.btn3.clicked.connect(lambda: self.game(0, 2, self.ui.btn3))
-        self.ui.btn4.clicked.connect(lambda: self.game(1, 0, self.ui.btn4))
-        self.ui.btn5.clicked.connect(lambda: self.game(1, 1, self.ui.btn5))
-        self.ui.btn6.clicked.connect(lambda: self.game(1, 2, self.ui.btn6))
-        self.ui.btn7.clicked.connect(lambda: self.game(2, 0, self.ui.btn7))
-        self.ui.btn8.clicked.connect(lambda: self.game(2, 1, self.ui.btn8))
-        self.ui.btn9.clicked.connect(lambda: self.game(2, 2, self.ui.btn9))
-        self.ui.btn_reset.clicked.connect(lambda: self.reset())
-
     def reset(self):
         self.cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         for btn in self.list_btn:
             btn.setText(None)    
-        self.ui.label.setText(None)
-        self.a = 0
-
+        self.counter = 0
+    
     def game(self, row, column, btn):
+        self.ui.label.clear()
         if self.cells[row][column] == " ":
-            self.a += 1
-            if self.a % 2 != 0:
+            self.counter += 1
+            if self.counter % 2 != 0:
                 btn.setText("X")
                 self.cells[row][column] = "X"
                 if self.is_winner("X"):
                     self.ui.label.setText('X WON')
                     self.reset()
-                if self.is_tie():
+                elif self.is_tie():
                     self.ui.label.setText("Tie game!")
+                    self.reset()
             else:
                 btn.setText("O")
                 self.cells[row][column] = "O"
                 if self.is_winner("O"):
                     self.ui.label.setText('O WON')
                     self.reset()
-                if self.is_tie():
+                elif self.is_tie():
                     self.ui.label.setText("Tie game!")
+                    self.reset()
         else:
             self.ui.label.setText("seat is taken")
-
-        def reset(self):
-            self.cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-            for btn in self.list_btn:
-                btn.setText(None)    
-            self.ui.label.setText(None)
-            self.a = 0
 
 
 if __name__ == "__main__":
